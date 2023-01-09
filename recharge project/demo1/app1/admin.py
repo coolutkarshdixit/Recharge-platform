196ea098f4c2bcffbb4f33116963af52be385b58
coolutkarshdixit <120125096+coolutkarshdixit@users.noreply.github.com>
2023-01-09 12:01:41 +0530

    Delete recharge project/demo1 directory

--- recharge project/demo1/app1/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from app1.models import ContactW
-from app1.models import ContactP
-
-admin.site.register(ContactW)
-admin.site.register(ContactP)
-
-# Register your models here.
